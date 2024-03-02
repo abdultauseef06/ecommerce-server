b90829638964b1c63982ae34cc650e3a086dd7e5
abdultauseef06 <abdultauseef.06@gmail.com>
2024-03-03 02:56:42 +0530

    made changes

--- utils/database.js
+++ utils/database.js
@@ -1,18 +1,18 @@
-const mongoose = require ('mongoose')
-//connecting to data base
-//const URI ="mongodb://127.0.0.1:27017/yubiApparels_admin"
-//mongoose.connect(URI);
+const mongoose = require('mongoose');
+
 const URI = process.env.MONGODB_URI;
-//const URI= "mongodb+srv://mohammedtauseef06:Abdul8118@cluster0.d4u806b.mongodb.net/yubiApparels_admin?retryWrites=true&w=majority"
-const connectdb = async()=>{
+
+const connectdb = async () => {
     try {
-        await mongoose.connect(URI)
-        console.log("connected succesfully to db");
+        await mongoose.connect(URI, {
+            useNewUrlParser: true,
+            useUnifiedTopology: true
+        });
+        console.log("Connected successfully to the database");
     } catch (error) {
-        console.error("database connection failed",error.message);
-        process.exit(0);
+        console.error("Database connection failed:", error.message);
+        process.exit(1); // Exit with non-zero code to indicate an error
     }
 };
-
 
 module.exports = connectdb;
